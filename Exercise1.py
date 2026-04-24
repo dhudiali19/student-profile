5c5cf06d6631a1589fcb577d7cba3789e7771b87
dhudiali19 <dhudiali19@gmail.com>
2026-04-24 10:18:18 +0100

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,66 @@
+x = 5
+y = "5"
+print(x + int(y) * 2)
+
+x = 10 
+y = x
+x = x + 5
+y = y * 2
+print(x, y)
+
+data = ["Python", 3, 8.5]
+a, b, c = data
+print(a * b)
+
+x = 10
+y = "20"
+print (x + int (y) )
+
+x = "Hello"
+def change():
+ x = "World"
+change()
+print(x)
+
+print("Age:", 20 + 5, "Years" + " Old")
+
+x = y = z = 10
+x += 5
+y *= 2
+z -= 3
+print(x, y, z)
+
+age = 25
+Age = 30
+print(age + Age)
+
+fruits = ["apple", "banana" , "orange"]
+x, y, z = fruits
+print (x, y, z)
+
+x = "3"
+y = 2
+z = float(x) * y + int(x)
+print(z)
+
+print("Hello", end="-")
+print("World", end="-")
+print("Python")
+
+x = "10"
+y = "5"
+print(int(x) + int(y) * int(x))
+
+x = 5
+x = x + 2
+x = x * x
+x = x - 3
+print(x)
+
+my_var = 10
+myVar = 20
+print(my_var + myVar)
+
+x, y = 5, 10
+x, y = y, x + y
+print(x, y)
